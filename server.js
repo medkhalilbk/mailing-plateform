7a0bcac54e119f87aa6da311eb478e644bcabd9c
medkhalilbk <medkhalilbenkhelil@gmail.com>
2023-04-25 05:07:22 +0100

    init server

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const fileUpload = require('express-fileupload');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const _ = require('lodash');
+const router = require('./routes');
+
+const app = express();
+
+// enable files upload 
+
+//add other middleware
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(morgan('dev'));
+
+//start app 
+const port = process.env.PORT || 3000;
+
+ 
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+ 
